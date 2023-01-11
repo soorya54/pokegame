c01d3f49752e8c64eda157cc13eb51219a9bc24d
Soorya <soorya@heptagon.in>
2023-01-11 15:57:24 +0530

    Battle boundary and collision added

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ for (let i = 0; i < collisions.length; i += 70) {
   collisionsMap.push(collisions.slice(i, i + 70));
 }
 
+const battleZonesMap = [];
+for (let i = 0; i < battleZonesData.length; i += 70) {
+  battleZonesMap.push(battleZonesData.slice(i, i + 70));
+}
+
 const offset = {
   x: -735,
   y: -650,
@@ -21,6 +26,22 @@ collisionsMap.forEach((row, i) => {
   row.forEach((symbol, j) => {
     if (symbol === 1025) {
       boundaries.push(
+        new Boundary({
+          position: {
+            x: j * Boundary.width + offset.x,
+            y: i * Boundary.height + offset.y,
+          },
+        })
+      );
+    }
+  });
+});
+
+const battleZones = [];
+battleZonesMap.forEach((row, i) => {
+  row.forEach((symbol, j) => {
+    if (symbol === 1025) {
+      battleZones.push(
         new Boundary({
           position: {
             x: j * Boundary.width + offset.x,
@@ -90,7 +111,7 @@ const keys = {
   },
 };
 
-const movables = [foreground, background, ...boundaries];
+const movables = [foreground, background, ...boundaries, ...battleZones];
 
 const rectangularCollision = ({ rectangle1, rectangle2 }) => {
   return (
@@ -107,8 +128,38 @@ const animate = () => {
   boundaries.forEach((boundary) => {
     boundary.draw();
   });
+  battleZones.forEach((battleZone) => {
+    battleZone.draw();
+  });
   player.draw();
   foreground.draw();
+
+  if (keys.w.pressed || keys.a.pressed || keys.s.pressed || keys.d.pressed) {
+    for (let i = 0; i < battleZones.length; i++) {
+      const battleZone = battleZones[i];
+      const overlappingArea =
+        (Math.min(
+          player.position.x + player.width,
+          battleZone.position.x + battleZone.width
+        ) -
+          Math.max(player.position.x, battleZone.position.x)) *
+        (Math.min(
+          player.position.y + player.height,
+          battleZone.position.y + battleZone.height
+        ) -
+          Math.max(player.position.y, battleZone.position.y));
+      if (
+        rectangularCollision({
+          rectangle1: player,
+          rectangle2: battleZone,
+        }) &&
+        overlappingArea > (player.width * player.height) / 2
+      ) {
+        console.log("BZ collision");
+        break;
+      }
+    }
+  }
 
   let moving = true;
   player.moving = false;
